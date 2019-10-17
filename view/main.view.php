bb9994ffef887db6f4d78730b263be78ac60b5f4
TAMAYO Enzo <chonchon3007@gmail.com>
2019-10-17 09:55:26 +0200

    html dans main.view

--- view/main.view.php
+++ view/main.view.php
@@ -1,3 +1,16 @@
 <?php
 //Page d'accueuil du site
 ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mish</title>
+  </head>
+  <body>
+    <a href="main.view.php">
+      <img src="design/MishHD.png" alt="Logo de Mish">
+    </a>
+  </body>
+</html>
