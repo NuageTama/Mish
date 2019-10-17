c0aad293ee43fb808543b4732258e6ff370805f1
NuageTama <chonchon3007@gmail.com>
2019-10-17 14:50:36 +0200

    Header et footer site, main.css

--- view/main.view.php
+++ view/main.view.php
@@ -1,16 +1,29 @@
-<?php
-//Page d'accueuil du site
-?>
-
+<!-- Page d'accueuil du site-->
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Mish</title>
+    <link rel="stylesheet" href="design/main.css">
   </head>
   <body>
-    <a href="main.view.php">
-      <img src="design/MishHD.png" alt="Logo de Mish">
-    </a>
+    <header>
+      <!--En-tête avec le logo et la navigation-->
+      <a href="main.view.php">
+        <img src="design/MishHD.png" alt="Logo de Mish" width="256">
+      </a>
+      <ul>
+        <li><a href="#">Chercher</a></li>
+        <li><a href="#">Categorie</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      </nav>
+    </header>
+    <main>
+      <!--Partie principale de la page-->
+    </main>
+    <footer>
+      <!--footer vide pour occuper l'espace-->
+    </footer>
   </body>
 </html>
