5dbe8efe6296eed4616d451738596ebb02fe528c
TAMAYO Enzo <chonchon3007@gmail.com>
2019-10-23 15:58:37 +0200

    Popup form test ver.

--- view/main.view.php
+++ view/main.view.php
@@ -4,13 +4,21 @@
   <head>
     <meta charset="utf-8">
     <title>Mish</title>
-    <link rel="stylesheet" href="design/main.css">
+    <link rel="stylesheet" href="../view/design/main.css">
   </head>
   <body>
     <header>
       <!--En-tête avec le logo et la navigation-->
+      <div class="iconBar">
+        <button id="login" onclick="openLoginForm()">
+          <img src="../view/design/icon_login.png" alt="login button" width="30px">
+        </button>
+        <button id="inscription" onclick="openInscriptionForm()">
+          <img src="../view/design/icon_create_account.png" alt="inscription button" width="30px">
+        </button>
+      </div>
       <a href="main.view.php">
-        <img src="design/MishHD.png" alt="Logo de Mish" width="256">
+        <img src="../view/design/MishHD.png" alt="Logo de Mish" width="256">
       </a>
       <ul>
         <li><a href="#">Chercher</a></li>
@@ -30,9 +38,32 @@
           </div>
         </div>
       <?php endforeach; ?>
+      <!--Popup form-->
+      <div class="form-popup" id="loginForm">
+
+      </div>
+      <div class="form-popup" id="inscriptionForm">
+
+      </div>
     </main>
     <footer>
       <!--footer vide pour occuper l'espace-->
     </footer>
+    <!--Javascript pour afficher les popups-->
+    <script>
+      function openLoginForm() {
+        document.getElementById("loginForm").style.display = "block";
+      }
+      function openInscriptionForm() {
+        document.getElementById("inscriptionForm").style.display = "block";
+      }
+      function closeLoginForm() {
+        document.getElementById("loginForm").style.display = "none";
+      }
+      function closeInscriptionForm() {
+        document.getElementById("inscriptionForm").style.display = "none";
+      }
+    </script>
+
   </body>
 </html>
